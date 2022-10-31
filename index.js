2cd7d19ef58a6f8ee4a7c6ff2035fdd00df9dd43
Scarchess <davilealmarcal198@gmail.com>
2022-10-30 23:53:44 -0300

    Atualização geral

--- index.js
+++ index.js
@@ -1,21 +1,68 @@
-const box1 = document.getElementById("box1");
-const box2 = document.getElementById("box2");
-const box3 = document.getElementById("box3");
-const box4 = document.getElementById("box4");
+const charCodes = generateCharCodes();
+const randomCharCodes = generateRandomCharCodes();
+const button = document.querySelector(".btn_password");
+var click = 0;
 
-var passwordKeys = []
 
-const passwordResult = []
+function generateCharCodes() {
 
-function generatePasswordKeys() {
-    s = '';
-        for( var i = 32; i <= 126; i++ )
-        {
-            s += String.fromCharCode( i );
-        }
+    const array = []
+
+    for (let i = 32; i < 232; i++) {
+        array.push(i);
+    }
+
+    return array
 }
 
+function generateRandomCharCodes(charCodes) {
+    
+    const array = []
+    const character = charCodes
+    for (let i = 0; i < 40; i++) {
+        array.push(Math.floor(Math.random(character) * 100));    
+    }
 
-function generatePassword() {
-
+    return array
 }
+
+const items = randomCharCodes //… your array, filled with values
+const n = 10 //tweak this to add more items per line
+
+const newResult = new Array(Math.ceil(items.length / n))
+  .fill()
+  .map(_ => items.splice(0, n))
+
+const result = newResult
+
+function buttonIsClicked(charCodes) {
+        generateCharCodes();
+        generateRandomCharCodes(charCodes);     
+}
+
+function numberToString(randomCharCodes, indice, array) {
+    strg = String.fromCharCode(randomCharCodes)
+    array[indice] = strg
+}
+
+boxx1 = result[0]
+boxx2 = result[1]
+boxx3 = result[2]
+boxx4 = result[3]
+
+boxx1.forEach(numberToString);
+boxx2.forEach(numberToString);
+boxx3.forEach(numberToString);
+boxx4.forEach(numberToString);
+
+
+button.addEventListener("click", () => {
+    document.getElementById("box1").innerHTML = boxx1.join(""),
+    document.getElementById("box2").innerHTML = boxx2.join(""),
+    document.getElementById("box3").innerHTML = boxx3.join(""),
+    document.getElementById("box4").innerHTML = boxx4.join(""),
+    buttonIsClicked()   
+})
+
+
+
